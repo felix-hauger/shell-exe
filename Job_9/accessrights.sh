caa7482a01830fecfc2dd23db04742ef976d1979
felix-hauger <felix.hauger@laplateforme.io>
2022-10-07 11:14:01 +0200

    removed some tests

--- Job_9/accessrights.sh
+++ Job_9/accessrights.sh
@@ -12,35 +12,24 @@ IFS=","
 while IFS="," read -r user_id user_firstname user_lastname user_password user_role
 do
 
-	echo "id : $user_id"
-
 #	DELETE EXISTING SPACES IN CSV FILE VARIABLES
 
 	user_firstname=${user_firstname//[[:blank:]]/}
-	echo "prénom : $user_firstname"
 
 	user_lastname=${user_lastname//[[:blank:]]/}
-	echo "nom de famille : $user_lastname"
 
 	user_name=$user_firstname$user_lastname
+
 	user_name=${user_name,,}
-	echo "user name : $user_name"
 
 	user_fullname="$user_firstname $user_lastname"
-	echo "nom complet : $user_fullname"
-
-#	echo "mot de passe : $user_password"
-
-	echo "rôle : $user_role"
 
 
 #	CHECK IF USER ID AND GROUP ID EXIST AND ASSIGN THEM TO USER IF THEY DON'T
 
 	id_passwd=$(sudo cat "/etc/passwd" | grep $user_id)
-	echo $id_passwd
 
 	id_group=$(sudo cat "/etc/group" | grep $user_id)
-	echo $id_group
 
 	if [[ -z $id_passwd && -z $id_group ]]
 	then
@@ -51,16 +40,14 @@ do
 		sudo useradd -c "$user_fullname" $user_name
 	fi
 
+#	REMOVE LAST CHARACTER OF LINE WITH %?
 
 	if [ ${user_role%?} = "Admin" ]
 	then
 		usermod -aG sudo $user_name
-		echo "proute"
 	fi
 
 	echo $user_name:$user_password | chpasswd
-
-	echo ""
 
 done < <(tail -n +2 Shell_Userlist.csv)
 
